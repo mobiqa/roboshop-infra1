de306c5dc7cf424e6c1d144f98ae5890a5912879
venkat <venkat.mobiqa@gmail.com>
2023-03-30 11:50:34 +0530

    updating ./roboshop-infra1

---
+++ env-dev/main.tfvars
@@ -0,0 +1,9 @@
+env = "dev"
+
+vpc = {
+  main = {
+    cidr_block = "10.0.0.0/16"
+
+  }
+
+}
---
+++ main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+  source = "github.com/mobiqa/tf-module-vpc1"
+
+  for_each = var.vpc
+  cidr_block = each.value.cidr_block
+}
